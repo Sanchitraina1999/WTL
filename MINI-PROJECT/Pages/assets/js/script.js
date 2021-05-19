3dd7929438446514427d77a5b2777ab6938a6af9
Sanchitraina1999 <sanchitraina1999@gmail.com>
2021-05-19 22:05:14 +0530

    update: master

--- MINI-PROJECT/Pages/assets/js/script.js
+++ MINI-PROJECT/Pages/assets/js/script.js
@@ -42,5 +42,6 @@ const takeName = () => {
 const checkIfLoggedIn = () => {
     if (localStorage.getItem("name")) {
         alert("Welcome, " + localStorage.getItem('name') + "!. You are being redirected to the homepage.")
+        window.location = "http://127.0.0.1:5500/MINI-PROJECT/Pages/AfterLoginPage.html";
     }
 }
