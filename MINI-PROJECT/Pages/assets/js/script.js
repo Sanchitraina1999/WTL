61f9113b02b865cb43633ce9c82a44b8237c63c7
Sanchitraina1999 <sanchitraina1999@gmail.com>
2021-05-19 22:01:44 +0530

    update: master

--- MINI-PROJECT/Pages/assets/js/script.js
+++ MINI-PROJECT/Pages/assets/js/script.js
@@ -34,6 +34,7 @@ const takeName = () => {
         return;
     }
     else {
+        localStorage.setItem("name", name);
         window.location = "http://127.0.0.1:5500/MINI-PROJECT/Pages/AfterLoginPage.html";
     }
 }
